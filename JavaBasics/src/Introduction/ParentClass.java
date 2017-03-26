50043e90558e37cab5fa7fd18964f565ef1a873d
sowmya2160 <sowmya2160@gmail.com>
2017-03-25 21:12:37 -0500

    Successfully commited

---
+++ JavaBasics/src/Introduction/ParentClass.java
@@ -0,0 +1,18 @@
+package Introduction;
+
+public class ParentClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+String s= "sowmya";
+String s1 = new String("sowmya");
+
+		
+	Person p = new Person();
+	p.hashCode();
+
+	}
+
+}
